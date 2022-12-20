abc57ea30425e430f70a0db4fe4d386770f4eb62
Pericles <113788303+OPericao@users.noreply.github.com>
2022-12-20 12:33:07 +0100

    Add files via upload

---
+++ P1/expr2.ml
@@ -0,0 +1,9 @@
+let u = 4 * 15 / 7 - 2
+
+let v = 2.0 *. asin 1.0
+
+let w = char_of_int (69 + 4)
+
+let x = false || (true && false)
+
+let y = if 10>18 then "mayor" else "menor";;
---
+++ P1/nombre.ml
@@ -0,0 +1,4 @@
+print_string "Lucas Piñeiro Añón";;
+print_endline "";;
+print_string "lucas.pineiro.anon@udc.es";;
+print_endline "";;
